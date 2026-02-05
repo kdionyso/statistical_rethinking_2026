41677ff5c746fd027134b6a26a2707a136fa5063
Kyriaki Dionysopoulou <kdionyso@gmail.com>
2026-02-05 14:18:54 +0000

    Checkpoint for last piece

--- b04_homework.R
+++ b04_homework.R
@@ -124,41 +124,42 @@ dashboard(mMLCF)
 mMLCF@cstanfit$cmdstan_diagnose()
 precis(mMLCF, depth = 2)
 
-# Mundlak model with confounding variable
-mMMCF <- ulam(
-    alist(
-        C ~ bernoulli(p),
-        logit(p) <- a[D] +
-            bK[D] *
-                sum(delta_j[1:K]) +
-            bA * A +
-            bU[D] * U,
-        bA ~ normal(0, 0.5),
-        transpars > vector[MAXD]:a <<- abar[1] + v[, 1],
-        transpars > vector[MAXD]:bK <<- abar[2] + v[, 2],
-        transpars > vector[MAXD]:bU <<- abar[3] + v[, 3],
-        # transpars > vector[MAXD]:bA <<- abar[4] + v[, 4],
+# # Mundlak model with confounding variable
+# mMMCF <- ulam(
+#     alist(
+#         C ~ bernoulli(p),
+#         logit(p) <- a[D] +
+#             bK[D] *
+#                 sum(delta_j[1:K]) +
+#             bA * A +
+#             bU[D] * U,
+#         bA ~ normal(0, 0.5),
+#         transpars > vector[MAXD]:a <<- abar[1] + v[, 1],
+#         transpars > vector[MAXD]:bK <<- abar[2] + v[, 2],
+#         transpars > vector[MAXD]:bU <<- abar[3] + v[, 3],
+#         # transpars > vector[MAXD]:bA <<- abar[4] + v[, 4],
 
-        transpars > matrix[MAXD, 3]:v <- compose_noncentered(sigma, L_Rho, Z),
-        vector[3]:abar ~ normal(0, 1),
-        matrix[3, MAXD]:Z ~ normal(0, 1),
-        vector[3]:sigma ~ exponential(1),
-        cholesky_factor_corr[3]:L_Rho ~ lkj_corr_cholesky(4),
-        vector[MAXK]:delta_j <<- append_row(0, delta),
-        simplex[MAXK - 1]:delta ~ dirichlet(alpha),
-        gq > matrix[3, 3]:Rho <<- Chol_to_Corr(L_Rho)
-    ),
-    data = dat,
-    chains = 4,
-    cores = 4,
-    cmdstan = TRUE,
-    log_lik = TRUE
-)
+#         transpars > matrix[MAXD, 3]:v <- compose_noncentered(sigma, L_Rho, Z),
+#         vector[3]:abar ~ normal(0, 1),
+#         matrix[3, MAXD]:Z ~ normal(0, 1),
+#         vector[3]:sigma ~ exponential(1),
+#         cholesky_factor_corr[3]:L_Rho ~ lkj_corr_cholesky(4),
+#         vector[MAXK]:delta_j <<- append_row(0, delta),
+#         simplex[MAXK - 1]:delta ~ dirichlet(alpha),
+#         gq > matrix[3, 3]:Rho <<- Chol_to_Corr(L_Rho)
+#     ),
+#     data = dat,
+#     chains = 4,
+#     cores = 4,
+#     cmdstan = TRUE,
+#     log_lik = TRUE
+# )
 
-dashboard(mMMCF)
-mMMCF@cstanfit$cmdstan_diagnose()
-precis(mMMCF, depth = 2)
+# dashboard(mMMCF)
+# mMMCF@cstanfit$cmdstan_diagnose()
+# precis(mMMCF, depth = 2)
 
+# Trial
 
 # Mundlak model with confounding variable
 dat <- list(
@@ -166,9 +167,9 @@ dat <- list(
     D = as.integer(d$district),
     U = as.integer(d$urban),
     A = standardize(d$age.centered),
-    # K = d$living.children, # use ordered data here
-    # alpha = rep(2, max(d$living.children) - 1),
-    # MAXK = max(d$living.children),
+    K = d$living.children, # use ordered data here
+    alpha = rep(2, max(d$living.children) - 1),
+    MAXK = max(d$living.children),
     MAXD = max(d$district)
     #    A = standardize(d$age.centered),
     #    K = d$living.children
@@ -176,7 +177,11 @@ dat <- list(
 mLMMCF <- ulam(
     alist(
         C ~ bernoulli(p),
-        logit(p) <- a[D] + bA * A + bU * U + bUG * UG[D],
+        logit(p) <- a[D] +
+            bA * A +
+            bU * U +
+            bK * sum(delta_j[1:K]) +
+            bUG * UG[D],
 
         # parameters for top model
         transpars > vector[MAXD]:a <<- abar + z * tau,
@@ -184,7 +189,7 @@ mLMMCF <- ulam(
         vector[MAXD]:z ~ normal(0, 1),
         tau ~ exponential(1),
 
-        # bK ~ normal(0, 1),
+        bK ~ normal(0, 1),
         bA ~ normal(0, 0.5),
         bU ~ normal(0, 1),
         bUG ~ normal(0, 1),
@@ -195,19 +200,19 @@ mLMMCF <- ulam(
         aU ~ normal(0, 1),
         bUD ~ exponential(1),
 
-        # # K model
-        # K ~ dordlogit(phi, cutpoints),
-        # phi <- aKK + bKK * UG[D],
-        # bKK ~ exponential(1),
-        # aKK ~ normal(0, 1),
-        # cutpoints ~ normal(2.5, 3),
+        # K model
+        K ~ ordered_logistic(phi, cutpoints),
+        phi <- aKK + bKK * UG[D],
+        bKK ~ exponential(1),
+        aKK ~ normal(0, 1),
+        cutpoints ~ normal(0, 1),
 
         # UG model
-        vector[MAXD]:UG ~ normal(0, 1) #,
+        vector[MAXD]:UG ~ normal(0, 1),
 
-        # # the rest
-        # vector[K]:delta_j <<- append_row(0, delta),
-        # simplex[K - 1]:delta ~ dirichlet(alpha) #,
+        # the rest
+        vector[MAXK]:delta_j <<- append_row(0, delta),
+        simplex[MAXK - 1]:delta ~ dirichlet(alpha) #,
         # # gq > matrix[2, 2]:Rho <<- Chol_to_Corr(L_Rho)
         #bK * sum(delta(1:K)) +
     ),
@@ -218,6 +223,6 @@ mLMMCF <- ulam(
     log_lik = TRUE
 )
 
-dashboard(mLMMCF)
-mLMMCF@cstanfit$cmdstan_diagnose()
-precis(mLMMCF, depth = 2)
+# dashboard(mLMMCF)
+# mLMMCF@cstanfit$cmdstan_diagnose()
+# precis(mLMMCF, depth = 2)
